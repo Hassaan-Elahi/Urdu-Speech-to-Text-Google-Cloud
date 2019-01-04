29f9fb738c56276f91bae2611b154e830e808531
Hassaan-Elahi <hassaan.elahi15@gmail.com>
2019-01-04 20:55:49 +0500

    Added Sound Fiels

---
+++ GoogleCloudSpeech.py
@@ -0,0 +1,16 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+audio = 'audio.wav'
+
+with sr.AudioFile(audio) as source:
+	audio = r.record(source)
+	print ('Done!')
+
+try:
+	text = r.recognize_google(audio)
+	print (text)
+
+except Exception as e:
+	print (e)
