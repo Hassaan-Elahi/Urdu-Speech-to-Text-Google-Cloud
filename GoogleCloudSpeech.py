355ed409828501296ae1a6a98ef00beb27b970f4
Hassaan-Elahi <hassaan.elahi15@gmail.com>
2019-01-04 22:02:04 +0500

    Added some code for automation

--- GoogleCloudSpeech.py
+++ GoogleCloudSpeech.py
@@ -1,16 +1,52 @@
-import speech_recognition as sr
+import io
+import os
+import codecs
 
-r = sr.Recognizer()
+# Imports the Google Cloud client library
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
 
-audio = 'audio.wav'
+# -------------------------------------------------------------------------------
+current_dir = "C:\\Users\\Hassan Elahi\\Desktop\\Urdu-Speech-to-Text-Google-Cloud\\DataSet\\wav\\"
+arr = list([])
+#--------------------------------------------------------------------------------
 
-with sr.AudioFile(audio) as source:
-	audio = r.record(source)
-	print ('Done!')
 
-try:
-	text = r.recognize_google(audio)
-	print (text)
 
-except Exception as e:
-	print (e)
+def Speech2Text(file):
+    
+    
+    file_name = current_dir + file
+    
+    # Loads the audio into memory
+    with io.open(file_name, 'rb') as audio_file:
+        content = audio_file.read()
+        audio = types.RecognitionAudio(content=content)
+    
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=16000,
+        language_code='ur-PK')
+    
+    # Detects speech in the audio file
+    response = client.recognize(config, audio)
+    
+    
+    text = " ".join([result.alternatives[0].transcript for result in response.results])
+    arr.append(file_name.split('\\')[-1]+":"+text)
+    print(file_name.split('\\')[-1]+":"+text)
+
+
+
+if __name__ == '__main__':
+    
+    #Setting Envionment vaiable for Google Credentials
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "E:/cred.json"
+    client = speech.SpeechClient()
+    Speech2Text("hin_0001.wav")
+    Speech2Text("hin_0002.wav")
+
+    with codecs.open(current_dir+'wav2Urdu.txt', 'w',encoding='utf-8') as f:
+        for item in arr:
+            f.write("{}\n".format(item))
